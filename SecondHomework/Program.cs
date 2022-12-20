f3407edd1ce1633121d2d4f83b65e732cbe8ae9a
Гиорги <avakov.giorgi@live.com>
2022-12-20 14:03:14 +0300

    Доработка по замечаниям

--- SecondHomework/Program.cs
+++ SecondHomework/Program.cs
@@ -28,62 +28,24 @@ namespace SecondHomework
             {
                 case 1:
                     Console.WriteLine("Заполняем List<int> значениями");
-                    AddValueInList();
-                    Console.WriteLine("\t\tПоиск числа 496753 в List<int>");                    
-                    stopwatch.Start();                    
-                    for (int j = 0; j < ProgramHelpers.Lisik.Count; j++)
-                    {
-                        if(ProgramHelpers.Lisik[j] == 496753)
-                        {
-                            stopwatch.Stop();
-                            break;
-                        }
-                        if(ProgramHelpers.Lisik[j]%777 == 0)
-                        {
-                            Console.WriteLine($"\t\t\tЧисло: {ProgramHelpers.Lisik[j]} делиться без отстатка на 777 => {stopwatch.Elapsed}");
-                        }
-                    }
-                    Console.WriteLine($"\t\t\tВремя поиска числа 496753 в List<int>: {stopwatch.Elapsed}");
+                    AddValueInList();                    
+                    Console.WriteLine("\t\tПоиск 496753-го элемента в List<int>");
+                    SearchItems(ProgramHelpers.Lisik, ProgramHelpers.Lisik.GetType());
+                    NoResidue(ProgramHelpers.Lisik, ProgramHelpers.Lisik.GetType(), 777);
                     break;
                 case 2:
                     Console.WriteLine("Заполняем ArrayList значениями");
                     AddValueInArrList();
-                    Console.WriteLine("\t\tПоиск числа 496753 в ArrayList");
-                    stopwatch.Start();
-                    for (int j = 0; j < ProgramHelpers.ArrayList.Count; j++)
-                    {
-                        if ((int)ProgramHelpers.ArrayList[j] == 496753)
-                        {
-                            stopwatch.Stop();
-                            break;
-                        }
-                        if ((int)ProgramHelpers.ArrayList[j] % 777 == 0)
-                        {
-                            Console.WriteLine($"\t\t\tЧисло: {ProgramHelpers.ArrayList[j]} делиться без отстатка на 777 => {stopwatch.Elapsed}");
-                        }
-                    }
-                    Console.WriteLine($"\t\t\tВремя поиска числа 496753 в ArrayList: {stopwatch.Elapsed}");
+                    Console.WriteLine("\t\tПоиск 496753-го элемента в ArrayList");
+                    SearchItems(ProgramHelpers.ArrayList, ProgramHelpers.ArrayList.GetType());
+                    NoResidue(ProgramHelpers.ArrayList, ProgramHelpers.ArrayList.GetType(), 777);
                     break;
                 case 3:
                     Console.WriteLine("Заполняем LinkList значениями");
                     AddValueInLinkList();
-                    Console.WriteLine("\t\tПоиск числа 496753 в LinkList");
-                    stopwatch.Start();
-                    var item = ProgramHelpers.LinkList.First;
-                    for (int j = 0; j < ProgramHelpers.LinkList.Count; j++)
-                    {                        
-                        if (item.Value == 496753)
-                        {
-                            stopwatch.Stop();
-                            break;
-                        }
-                        if (item.Value % 777 == 0)
-                        {
-                            Console.WriteLine($"\t\t\tЧисло: {item.Value} делиться без отстатка на 777 => {stopwatch.Elapsed}");
-                        }
-                        item = item.Next;
-                    }
-                    Console.WriteLine($"\t\t\tВремя поиска числа 496753 в LinkList: {stopwatch.Elapsed}");
+                    Console.WriteLine("\t\tПоиск 496753-го элемента в LinkList");
+                    SearchItems(ProgramHelpers.LinkList, ProgramHelpers.LinkList.GetType());
+                    NoResidue(ProgramHelpers.LinkList, ProgramHelpers.LinkList.GetType(), 777);
                     break;
                 default:
                     break;
@@ -102,27 +64,117 @@ namespace SecondHomework
         }
         static void AddValueInArrList()
         {
-            Stopwatch stopwatch1 = new Stopwatch();
-            stopwatch1.Start();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
             for (int i = 1; i <= 1000000; i++)
             {
                 ProgramHelpers.ArrayList.Add(i);
             }
-            stopwatch1.Stop();
-            Console.WriteLine($"\tВремя заполнения ArrayList: {stopwatch1.Elapsed}");
+            stopwatch.Stop();
+            Console.WriteLine($"\tВремя заполнения ArrayList: {stopwatch.Elapsed}");
         }
         static void AddValueInLinkList()
         {
-            Stopwatch stopwatch2 = new Stopwatch();
-            stopwatch2.Start();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
             for (int i = 1; i <= 1000000; i++)
             {
                 ProgramHelpers.LinkList.AddLast(i);
             }
-            stopwatch2.Stop();
-            Console.WriteLine($"\tВремя заполнения LinkList<int>: {stopwatch2.Elapsed}");
+            stopwatch.Stop();
+            Console.WriteLine($"\tВремя заполнения LinkList<int>: {stopwatch.Elapsed}");
         }
-
-
+        static void SearchItems(object list, Type listType)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            var type = listType.ToString();
+            switch (type)
+            {
+                case "System.Collections.Generic.List`1[System.Int32]":
+                    stopwatch.Start();
+                    var forListCycle = list as List<int>;
+                    for (int i = 0; i < forListCycle.Count; i++)
+                    {
+                        if (Math.Abs(i) == 496753)
+                        {
+                            stopwatch.Stop();
+                            Console.WriteLine($"\t\t\tВремя поиска 496753-го элемента в List<int>: {stopwatch.Elapsed}");
+                            break;
+                        }
+                    }                    
+                    break;
+                case "System.Collections.Generic.LinkedList`1[System.Int32]":
+                    stopwatch.Start();
+                    var forLinkedListcycle = list as LinkedList<int>;
+                    var item = forLinkedListcycle.First;
+                    for (int i = 0; i < forLinkedListcycle.Count; i++)
+                    {
+                        if (Math.Abs(i) == 496753)
+                        {
+                            stopwatch.Stop();
+                            Console.WriteLine($"\t\t\tВремя поиска 496753-го элемента в LinkList: {stopwatch.Elapsed}");
+                            break;
+                        }
+                        item = item.Next;
+                    }
+                    break;
+                case "System.Collections.ArrayList":
+                    stopwatch.Start();
+                    var forArrayListCycle = list as ArrayList;
+                    for (int i = 0; i < forArrayListCycle.Count; i++)
+                    {
+                        if (Math.Abs(i) == 496753)
+                        {
+                            stopwatch.Stop();
+                            Console.WriteLine($"\t\t\tВремя поиска 496753-го элемента в List<int>: {stopwatch.Elapsed}");
+                            break;
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        static void NoResidue(object list, Type listType, int divisor)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            var type = listType.ToString();
+            switch (type)
+            {
+                case "System.Collections.Generic.List`1[System.Int32]":
+                    var forListCycle = list as List<int>;
+                    stopwatch.Start();
+                    for (int i = 0; i < forListCycle.Count; i++)
+                    {
+                        if (forListCycle[i]%divisor == 0)
+                            Console.WriteLine(forListCycle[i]);
+                    }                    
+                    break;
+                case "System.Collections.Generic.LinkedList`1[System.Int32]":
+                    stopwatch.Start();
+                    var forLinkedListcycle = list as LinkedList<int>;
+                    var item = forLinkedListcycle.First;
+                    for (int i = 0; i < forLinkedListcycle.Count; i++)
+                    {
+                        if(item.Value%divisor==0)
+                            Console.WriteLine(item.Value);
+                        item = item.Next;
+                    }
+                    break;
+                case "System.Collections.ArrayList":
+                    stopwatch.Start();
+                    var forArrayListCycle = list as ArrayList;
+                    for (int i = 0; i < forArrayListCycle.Count; i++)
+                    {
+                        if((int)forArrayListCycle[i]%divisor == 0)
+                            Console.WriteLine(forArrayListCycle[i]);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine($"Время поиска чисел еоторые деляться без остатка на 777 {stopwatch.Elapsed}");
+            stopwatch.Stop();
+        }
     }
 }
